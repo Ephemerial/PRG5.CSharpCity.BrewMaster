6a433c60d358dbf38c325127ab4b4a899f1b5b8d
Eric Kuijpers <egm.kuijpers1@avans.nl>
2020-09-02 15:08:10 +0200

    Changed variable name

--- PRG5.CSharpCity.BrewMaster/Program.cs
+++ PRG5.CSharpCity.BrewMaster/Program.cs
@@ -11,46 +11,46 @@ namespace PRG5.CSharpCity.BrewMaster
             JuraBrew KoffieMachine = new JuraBrew();
             string commandKey = Console.ReadLine();
 
-            while (!commandKey.Equals("X"))
+            while (!commandKey.Equals("X",StringComparison.OrdinalIgnoreCase))
             {
                 switch (commandKey)
                 {
                     case "1":
-                        koffiemachine.SoortBakkie = KoffieSoort.Koffie;
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.Koffie;
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                     case "2":
-                        koffiemachine.SoortBakkie = KoffieSoort.Cappucino;
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.Cappucino;
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                     case "3":
-                        koffiemachine.SoortBakkie = KoffieSoort.Espresso;
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.Espresso;
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                     case "4":
-                        koffiemachine.SoortBakkie = KoffieSoort.Ristretto;
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.Ristretto;
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                     case "5":
-                        koffiemachine.SoortBakkie = KoffieSoort.AvansSlootwater;
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.AvansSlootwater;
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                     case "O":
-                        koffiemachine.Ontkalken();
+                        KoffieMachine.Ontkalken();
                         break;
                     default:
-                        koffiemachine.SoortBakkie = KoffieSoort.Koffie;
-                        Console.WriteLine($"Dat hebben we niet, u krijgt gewoon {koffiemachine.SoortBakkie}");
-                        Console.WriteLine(koffiemachine.Brew());
+                        KoffieMachine.SoortBakkie = KoffieSoort.Koffie;
+                        Console.WriteLine($"Dat hebben we niet, u krijgt gewoon {KoffieMachine.SoortBakkie}");
+                        Console.WriteLine(KoffieMachine.Brew());
                         break;
                 }
                 commandKey = Console.ReadLine();
             }
 
-            Console.WriteLine($"Ik heb {koffiemachine.GezetteBakjes.Count} bakjes gezet;");
-            Console.WriteLine($"Ik heb {koffiemachine.GezetteBakjes.Where(b => b == KoffieSoort.Cappucino).Count()} bakjes gezet;");
+            Console.WriteLine($"Ik heb {KoffieMachine.GezetteBakjes.Count} bakjes gezet;");
+            Console.WriteLine($"Ik heb {KoffieMachine.GezetteBakjes.Where(b => b == KoffieSoort.Cappucino).Count()} bakjes gezet;");
 
-            foreach (KoffieSoort bakje in koffiemachine.GezetteBakjes)
+            foreach (KoffieSoort bakje in KoffieMachine.GezetteBakjes)
             {
                 Console.WriteLine(bakje);
             }
