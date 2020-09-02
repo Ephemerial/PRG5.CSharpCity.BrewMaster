4a5b2a28560be5284747581e6e4571f40d2a0941
Eric Kuijpers <egm.kuijpers1@avans.nl>
2020-09-02 11:56:05 +0200

    Add project files.

---
+++ PRG5.CSharpCity.BrewMaster.Domain/JuraBrew.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRG5.CSharpCity.BrewMaster.Domain
+{
+    public enum KoffieSoort { 
+        Koffie,
+        Cappucino,
+        Espresso,
+        Ristretto,
+        AvansSlootwater
+    }
+
+    public class JuraBrew
+    {
+        private List<KoffieSoort> _gezetteBakjes;
+        private int _aantalBakjes;
+
+        public JuraBrew()
+        { 
+            _aantalBakjes = 0;
+            SoortBakkie = KoffieSoort.Espresso;
+            _gezetteBakjes = new List<KoffieSoort>();
+        }
+
+        public List<KoffieSoort> GezetteBakjes => _gezetteBakjes;
+
+        public KoffieSoort SoortBakkie { get; set; }
+
+        public int AantalBakjes {
+            get {
+                return _aantalBakjes;
+            }
+        }
+
+        public bool Ontkalken()
+        {
+            if (_aantalBakjes >= 5)
+            {
+                _aantalBakjes = 0;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public string Brew()
+        {
+            if (_aantalBakjes < 5)
+            {
+                _aantalBakjes++;
+                _gezetteBakjes.Add(SoortBakkie);
+                return $"Alstublieft een lekker bakkie {SoortBakkie}";
+            }
+            else 
+            {
+                return "U moet eerst ontkalken.";
+            }
+            
+        }
+    }
+}
---
+++ PRG5.CSharpCity.BrewMaster/Program.cs
@@ -0,0 +1,62 @@
+﻿using PRG5.CSharpCity.BrewMaster.Domain;
+using System;
+using System.Linq;
+
+namespace PRG5.CSharpCity.BrewMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuraBrew KoffieMachine = new JuraBrew();
+            string commandKey = Console.ReadLine();
+
+            while (!commandKey.Equals("X"))
+            {
+                switch (commandKey)
+                {
+                    case "1":
+                        koffiemachine.SoortBakkie = KoffieSoort.Koffie;
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                    case "2":
+                        koffiemachine.SoortBakkie = KoffieSoort.Cappucino;
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                    case "3":
+                        koffiemachine.SoortBakkie = KoffieSoort.Espresso;
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                    case "4":
+                        koffiemachine.SoortBakkie = KoffieSoort.Ristretto;
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                    case "5":
+                        koffiemachine.SoortBakkie = KoffieSoort.AvansSlootwater;
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                    case "O":
+                        koffiemachine.Ontkalken();
+                        break;
+                    default:
+                        koffiemachine.SoortBakkie = KoffieSoort.Koffie;
+                        Console.WriteLine($"Dat hebben we niet, u krijgt gewoon {koffiemachine.SoortBakkie}");
+                        Console.WriteLine(koffiemachine.Brew());
+                        break;
+                }
+                commandKey = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ik heb {koffiemachine.GezetteBakjes.Count} bakjes gezet;");
+            Console.WriteLine($"Ik heb {koffiemachine.GezetteBakjes.Where(b => b == KoffieSoort.Cappucino).Count()} bakjes gezet;");
+
+            foreach (KoffieSoort bakje in koffiemachine.GezetteBakjes)
+            {
+                Console.WriteLine(bakje);
+            }
+
+            Console.WriteLine("Bedankt voor het drinken van Jura Brew!");
+            Console.ReadLine();
+        }
+    }
+}
